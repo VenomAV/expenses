1583bc18128e2ed8b6575e7b2a2bde00d9950b45
Andrea Vallotti <andrea.vallotti@gmail.com>
2018-07-07 11:19:42 +0200

    First implementation of addToExpense and claim

--- src/main/scala/Expenses/ApplicationServices/ExpenseApplicationService.scala
+++ src/main/scala/Expenses/ApplicationServices/ExpenseApplicationService.scala
@@ -1,11 +1,11 @@
 package Expenses.ApplicationServices
 
 import Expenses.Model.Employee.EmployeeId
-import Expenses.Model.Expense
+import Expenses.Model.{Expense, OpenExpenseSheet}
 import Expenses.Model.ExpenseSheet.ExpenseSheetId
 import Expenses.Repositories.{ClaimRepository, EmployeeRepository, ExpenseSheetRepository}
 import Expenses.Services.ExpenseService
-import Expenses.Utils.ErrorManagement.ApplicationResult
+import Expenses.Utils.ErrorManagement.{ApplicationResult, ErrorList }
 import Expenses.Utils.ErrorManagement.implicits._
 import cats._
 import cats.implicits._
@@ -21,9 +21,30 @@ object ExpenseApplicationService {
       .flatMap(_.traverse(esr.save(_)))
 
   def addExpenseTo[F[_]](expense: Expense, id: ExpenseSheetId)
-                        (implicit esr: ExpenseSheetRepository[F]) : F[ApplicationResult[Unit]] = ???
+                        (implicit M:Monad[F],
+                         esr: ExpenseSheetRepository[F]) : F[ApplicationResult[Unit]] =
+    esr.get(id)
+      .map(_.orError("Unable to find expense sheet"))
+      .map(_.flatMap({
+        case oes: OpenExpenseSheet => Right(oes)
+        case _ => Left(ErrorList.of(s"$id is not an open expense sheet"))
+      }))
+      .map(_.flatMap(ExpenseService.addExpenseTo(expense, _).toEither))
+      .flatMap(_.traverse(esr.save(_)))
 
   def claim[F[_]](id: ExpenseSheetId)
-                 (implicit esr: ExpenseSheetRepository[F],
-                  cr: ClaimRepository[F]) : F[ApplicationResult[Unit]] = ???
+                 (implicit M:Monad[F],
+                  esr: ExpenseSheetRepository[F],
+                  cr: ClaimRepository[F]) : F[ApplicationResult[Unit]] =
+    esr.get(id)
+      .map(_.orError("Unable to find expense sheet"))
+      .map(_.flatMap({
+        case oes: OpenExpenseSheet => Right(oes)
+        case _ => Left(ErrorList.of(s"$id is not an open expense sheet"))
+      }))
+      .map(_.flatMap(ExpenseService.claim(_).toEither))
+      .flatMap(_.traverse(pair => {
+        cr.save(pair._2)
+        esr.save(pair._1)
+      }))
 }
