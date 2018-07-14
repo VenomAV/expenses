6813ddbb041e0f0d4c0d424f724fc58e235d9790
Andrea Vallotti <andrea.vallotti@gmail.com>
2018-07-14 10:39:06 +0200

    Add test on ExpenseApplicationService

--- src/test/scala/Expenses/ApplicationService/ExpenseApplicationServiceTest.scala
+++ src/test/scala/Expenses/ApplicationService/ExpenseApplicationServiceTest.scala
@@ -43,18 +43,53 @@ class ExpenseApplicationServiceTest extends FunSpec with Matchers with BeforeAnd
       }
     }
   }
+  describe("addExpenseTo") {
+    it("should add an expense to an open expense sheet") {
+      val employee = Employee(UUID.randomUUID(), "A", "V")
+      val expense = TravelExpense(Money(1, "EUR"), new Date(), "Florence", "Barcelona")
+      val expenseSheet = OpenExpenseSheet(UUID.randomUUID(), employee, List())
+      val state = TestState(List(employee), List(expenseSheet), List())
+      val id = expenseSheet.id
+      val newState = runAddExpenseTo(expense, id, state)._1
+
+      newState.expenseSheets should matchPattern {
+        case List(OpenExpenseSheet(`id`, `employee`, List(`expense`))) =>
+      }
+    }
+    it("should return error when expense sheet is missing") {
+      val expense = TravelExpense(Money(1, "EUR"), new Date(), "Florence", "Barcelona")
+      val state = TestState(List(), List(), List())
+      val (newState, result) = runAddExpenseTo(expense, UUID.randomUUID(), state)
+
+      newState should equal(state)
+      result should matchPattern {
+        case Left(NonEmptyList("Unable to find expense sheet", _)) =>
+      }
+    }
+    it("should not add an expense to a claimed expense sheet") {
+      val employee = Employee(UUID.randomUUID(), "A", "V")
+      val expense = TravelExpense(Money(1, "EUR"), new Date(), "Florence", "Barcelona")
+      val expenseSheet = ClaimedExpenseSheet(UUID.randomUUID(), employee, List(expense))
+      val state = TestState(List(employee), List(expenseSheet), List())
+      val (newState, result) = runAddExpenseTo(expense, expenseSheet.id, state)
+
+      result should matchPattern {
+        case Left(NonEmptyList(s, _)) if s.toString.endsWith(" is not an open expense sheet") =>
+      }
+      newState.expenseSheets should matchPattern {
+        case List(`expenseSheet`) =>
+      }
+    }
+  }
   describe("claim") {
     it("should save claimed expense sheet and pending claim") {
       val employee = Employee(UUID.randomUUID(), "A", "V")
       val expenses = List(TravelExpense(Money(1, "EUR"), new Date(), "Florence", "Barcelona"))
-      val openExpenseSheet = OpenExpenseSheet(UUID.randomUUID(), employee, expenses)
-      val state = TestState(List(employee), List(openExpenseSheet), List())
-      val id = openExpenseSheet.id
-      val (newState, result) = runClaimFor(id, state)
+      val expenseSheet = OpenExpenseSheet(UUID.randomUUID(), employee, expenses)
+      val state = TestState(List(employee), List(expenseSheet), List())
+      val id = expenseSheet.id
+      val newState = runClaim(id, state)._1
 
-      result should matchPattern {
-        case Right(()) =>
-      }
       newState.claims should matchPattern {
         case List(PendingClaim(_, `employee`, _)) =>
       }
@@ -67,6 +102,9 @@ class ExpenseApplicationServiceTest extends FunSpec with Matchers with BeforeAnd
   private def runOpenFor(employeeId: EmployeeId, state: TestState) : (TestState, ApplicationResult[Unit]) =
     ExpenseApplicationService.openFor[Test](employeeId).run(state).value
 
-  private def runClaimFor(expenseSheetId: ExpenseSheetId, state: TestState) : (TestState, ApplicationResult[Unit]) =
+  private def runAddExpenseTo(expense: Expense, expenseSheetId: ExpenseSheetId, state: TestState) =
+    ExpenseApplicationService.addExpenseTo[Test](expense, expenseSheetId).run(state).value
+
+  private def runClaim(expenseSheetId: ExpenseSheetId, state: TestState) : (TestState, ApplicationResult[Unit]) =
     ExpenseApplicationService.claim[Test](expenseSheetId).run(state).value
 }
